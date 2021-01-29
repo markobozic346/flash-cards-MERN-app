c48e513baa3609bc25a4639f68ea4b4b177d82bf
markobozic346 <55995892+markobozic346@users.noreply.github.com>
2021-01-30 00:09:24 +0100

    rotate card on hover

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,21 +1,30 @@
-import React from "react";
+import React, { useState }from "react";
 import "./Card.css"
 
+
 function Card() {
+     
+    const [degress, setState] = useState(0);
+    
+   const rotateStyles = []
     return( 
         <>
         <div className="card container center">
             <div className="front content">
                 <h1>This is front-side</h1>
             </div>
-            <div className="back content">
+            <div className="back content" >
                 <h1>This is back-side</h1>
             </div>
         </div>
         <div className="cards-navigation center">
             <button>❌</button>
-            <button>⭮</button>
+            <button onClick={()=>{
+               
+                
+            }}>⭮</button>
             <button>✔️</button>
+            
         </div>
         </>
     )
