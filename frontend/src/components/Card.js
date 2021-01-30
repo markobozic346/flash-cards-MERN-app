8ff957c45364682f904cc5bb6c48a905ad589ea7
markobozic346 <55995892+markobozic346@users.noreply.github.com>
2021-01-31 00:31:18 +0100

    rotate card on btn click

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -4,25 +4,22 @@ import "./Card.css"
 
 function Card() {
      
-    const [degress, setState] = useState(0);
+    const [cardSide, setCardSide] = useState("0");
     
-   const rotateStyles = []
+   
     return( 
         <>
-        <div className="card container center">
-            <div className="front content">
+        <div className="card container center" >
+            <div className="front content" rotate = {cardSide}>
                 <h1>This is front-side</h1>
             </div>
-            <div className="back content" >
+            <div className="back content" rotate = {cardSide}>
                 <h1>This is back-side</h1>
             </div>
         </div>
         <div className="cards-navigation center">
             <button>❌</button>
-            <button onClick={()=>{
-               
-                
-            }}>⭮</button>
+            <button onClick={() => {setCardSide(cardSide == '0' || cardSide =='2' ? '1' : '2')}}>⭮</button>
             <button>✔️</button>
             
         </div>
